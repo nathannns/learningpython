4f64cb135cbab790270e2bab34b2de35a9bd3d4a
Nathannsss <97411468+nathannns@users.noreply.github.com>
2022-11-29 10:47:21 +0100

    Add files via upload

---
+++ variable test.py
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age = "50"
+
+
+print("there once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike"
+print("he really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
---
+++ working with strings.py
@@ -0,0 +1,9 @@
+phrase = "Nathan Academy"
+print (phrase + " is\ncool")
+print(phrase.islower() )
+print(phrase.upper().isupper() )
+print(len(phrase))
+print (phrase[0])
+print(phrase. index("t"))
+print(phrase. index("Aca"))
+print(phrase. replace("Nathan", "Santiaji"))
